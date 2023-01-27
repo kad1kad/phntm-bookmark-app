956f540062675b5cd9c67dda80cdcc312d022c6c
kad1kad <k.aliu@me.com>
2023-01-27 16:41:17 +0000

    btn styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Form({ linkTitle, setLinkTitle, link, setLink, handleSubmit }) {
   return (
-    <div>
+    <div className="mt-5">
       <form action="submit" onSubmit={handleSubmit}>
         <input
           type="text"
@@ -19,7 +19,12 @@ function Form({ linkTitle, setLinkTitle, link, setLink, handleSubmit }) {
           onChange={(e) => setLinkTitle(e.target.value)}
         />
 
-        <button type="submit">Add</button>
+        <button
+          className="bg-slate-900 text-slate-50 font-light tracking-wider w-28 h-10 rounded-3xl"
+          type="submit"
+        >
+          Add
+        </button>
       </form>
     </div>
   );

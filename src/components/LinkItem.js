becf09e4a6c8d65753cfa330540bee948bc5070f
kad1kad <k.aliu@me.com>
2023-01-28 10:31:54 +0000

    add framer motion, lift up linkArr.map

--- src/components/LinkItem.js
+++ src/components/LinkItem.js
@@ -1,20 +1,36 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-function LinkItem({ linkArr, onRemove }) {
+function LinkItem({ onRemove, link, index }) {
   return (
-    <div>
-      {linkArr.map((link, index) => (
-        <div
-          className="p-2 flex justify-between items-center mt-5 shadow-lg rounded-lg"
-          key={link.link}
-        >
-          <a className="" href={link.link} target="_blank" rel="noreferrer">
-            {link.linkTitle}
-          </a>
-          <button onClick={() => onRemove(index)}>Remove</button>
-        </div>
-      ))}
-    </div>
+    <section className="flex items-center h-16 gap-1 mt-2">
+      <motion.a
+        href={link.link}
+        className="p-2 flex justify-between items-center bg-slate-900 text-slate-100 rounded-3xl flex-1 h-10"
+        key={link.link}
+        target="_blank"
+        rel="noreferrer"
+        whileHover={{ scale: 1.01 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0, y: -100 }}
+        exit={{ y: 500 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ type: "spring", stiffness: 100 }}
+      >
+        {link.linkTitle}
+      </motion.a>
+
+      <motion.button
+        className="bg-slate-900 h-10 rounded-3xl text-slate-100 text-sm px-1"
+        onClick={() => onRemove(index)}
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ type: "spring", stiffness: 100 }}
+        exit={{ x: -50 }}
+      >
+        Remove
+      </motion.button>
+    </section>
   );
 }
 

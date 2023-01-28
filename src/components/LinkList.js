a5b5d0b0b6df5924157438182b075b8ad01d1476
kad1kad <k.aliu@me.com>
2023-01-28 13:56:34 +0000

    add pagination logic

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,15 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import LinkItem from "./LinkItem";
+import Pagination from "./Pagination";
 
-function LinkList({ linkArr, onRemove }) {
+function LinkList({ linkArr, setLinkArr }) {
   // Re-render when linkArr changes
   useEffect(() => {
     console.log("Link list updated");
   }, [linkArr]);
 
+  // start at page 1
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const linksPerPage = 3;
+
+  const onRemove = (index) => {
+    // Remove link from array
+    const newLinkArr = linkArr.filter((link, i) => i !== index);
+    setLinkArr(newLinkArr);
+    // Check if the current page should be updated
+    if ((currentPage - 1) * linksPerPage >= newLinkArr.length) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  // Get current links
+  const indexOfLastLink = currentPage * linksPerPage;
+  const indexOfFirstLink = indexOfLastLink - linksPerPage;
+  const currentLinks = linkArr.slice(indexOfFirstLink, indexOfLastLink);
+
   return (
     <div>
-      {linkArr.map((link, index) => (
+      {currentLinks.map((link, index) => (
         <LinkItem
           linkArr={linkArr}
           onRemove={onRemove}
@@ -17,6 +38,15 @@ function LinkList({ linkArr, onRemove }) {
           index={index}
         />
       ))}
+
+      <div className="flex justify-center">
+        <Pagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          linkArr={linkArr}
+          linksPerPage={linksPerPage}
+        />
+      </div>
     </div>
   );
 }

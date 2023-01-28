f1a367ab5475dd170669fe4551862bf910589efd
kad1kad <k.aliu@me.com>
2023-01-28 13:56:16 +0000

    refactor

--- src/components/LinkItem.js
+++ src/components/LinkItem.js
@@ -3,11 +3,10 @@ import { motion } from "framer-motion";
 
 function LinkItem({ onRemove, link, index }) {
   return (
-    <section className="flex items-center h-16 gap-1 mt-2">
+    <div className="flex items-center h-16 gap-1 mt-2" key={[link.link, index]}>
       <motion.a
         href={link.link}
         className="p-2 flex justify-between items-center bg-slate-900 text-slate-100 rounded-3xl flex-1 h-10"
-        key={link.link}
         target="_blank"
         rel="noreferrer"
         whileHover={{ scale: 1.01 }}
@@ -16,6 +15,7 @@ function LinkItem({ onRemove, link, index }) {
         exit={{ y: 500 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ type: "spring", stiffness: 100 }}
+        viewport={{ once: true }}
       >
         {link.linkTitle}
       </motion.a>
@@ -27,10 +27,11 @@ function LinkItem({ onRemove, link, index }) {
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ type: "spring", stiffness: 100 }}
         exit={{ x: -50 }}
+        viewport={{ once: true }}
       >
         Remove
       </motion.button>
-    </section>
+    </div>
   );
 }
 

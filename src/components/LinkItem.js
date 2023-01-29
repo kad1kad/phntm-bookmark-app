f691fdb8c21673b0dcc9869e0c3e055fc7592daa
kad1kad <k.aliu@me.com>
2023-01-29 11:17:18 +0000

    add edit fuctionality

--- src/components/LinkItem.js
+++ src/components/LinkItem.js
@@ -1,36 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import EditLinkModal from "./EditLinkModal";
 
-function LinkItem({ onRemove, link, index }) {
+function LinkItem({
+  onRemove,
+  link,
+  index,
+  currentPage,
+  linksPerPage,
+  linkArr,
+  setLinkArr,
+}) {
+  // state for the modal
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
   return (
-    <div className="flex items-center h-16 gap-1 mt-2" key={[link.link, index]}>
+    <div className="flex items-center mt-2 relative">
       <motion.a
         href={link.link}
-        className="p-2 flex justify-between items-center bg-slate-900 text-slate-100 rounded-3xl flex-1 h-10"
+        className="flex justify-center items-center bg-slate-900 text-slate-100 rounded-3xl flex-1 h-32 tracking-wide"
         target="_blank"
         rel="noreferrer"
-        whileHover={{ scale: 1.01 }}
+        whileHover={{ scale: 1.04 }}
         whileTap={{ scale: 0.95 }}
         initial={{ opacity: 0, y: -100 }}
-        exit={{ y: 500 }}
-        whileInView={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: 1, y: 0, x: 0 }}
         transition={{ type: "spring", stiffness: 100 }}
         viewport={{ once: true }}
       >
-        {link.linkTitle}
+        <motion.span
+          initial={{ opacity: 0, y: -80 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ type: "spring", stiffness: 130 }}
+          viewport={{ once: true }}
+        >
+          {link.linkTitle}
+        </motion.span>
       </motion.a>
 
+      {/* Edit button */}
       <motion.button
-        className="bg-slate-900 h-10 rounded-3xl text-slate-100 text-sm px-1"
-        onClick={() => onRemove(index)}
-        initial={{ opacity: 0, x: -50 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ type: "spring", stiffness: 100 }}
-        exit={{ x: -50 }}
+        className="rounded-3xl text-sm px-1 absolute bottom-2 right-2 flex justify-center items-center"
+        onClick={() => {
+          handleOpen();
+        }}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0, y: -100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ type: "spring", stiffness: 80 }}
         viewport={{ once: true }}
       >
-        Remove
+        <EditOutlined style={{ fontSize: "18px", color: "#dbedec" }} />
       </motion.button>
+
+      {/* Delete button */}
+      <motion.button
+        className="rounded-3xl text-sm px-1 absolute bottom-2 left-2 flex justify-center items-center"
+        onClick={() => onRemove(index + (currentPage - 1) * linksPerPage)}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0, y: -100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ type: "spring", stiffness: 80 }}
+        viewport={{ once: true }}
+      >
+        <DeleteOutlined style={{ fontSize: "18px", color: "#dbedec" }} />
+      </motion.button>
+
+      {/* EditLinkModal component */}
+      {isOpen && (
+        <EditLinkModal
+          link={link}
+          linkTitle={link.linkTitle}
+          index={index}
+          handleClose={handleClose}
+          setLinkArr={setLinkArr}
+          linkArr={linkArr}
+        />
+      )}
     </div>
   );
 }

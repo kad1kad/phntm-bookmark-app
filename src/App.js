539b1ae4097b0bf839fb5292d958781bdeb883f4
kad1kad <k.aliu@me.com>
2023-01-29 11:15:30 +0000

    add url validation and delete all links btn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import isReachable from "is-reachable";
 import { useState } from "react";
+import DeleteAllLinks from "./components/DeleteAllLinks";
 import Form from "./components/Form";
 import LinkList from "./components/LinkList";
 
@@ -7,35 +8,55 @@ function App() {
   // State for Form comp
   const [link, setLink] = useState("");
   const [linkTitle, setLinkTitle] = useState("");
-  const [linkArr, setLinkArr] = useState([]);
+  // check local storage for prev links
+  const [linkArr, setLinkArr] = useState(
+    JSON.parse(localStorage.getItem("prevLinkArr")) || []
+  );
+
   const [error, setError] = useState("");
 
-  function ClearAll() {
-    setLinkArr([]);
+  async function urlExists(link) {
+    try {
+      const absoluteLink = "//" + link;
+      const result = await fetch(absoluteLink, { method: "HEAD" });
+      if (!result.ok) return false;
+      if (result.status >= 200 && result.status < 300) return true;
+      return false;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
   }
-
-  // const onRemove = (index) => {
-  //   // Remove link from array
-  //   const newLinkArr = linkArr.filter((link, i) => i !== index);
-  //   setLinkArr(newLinkArr);
-  // };
 
   async function handleSubmit(e) {
     e.preventDefault();
-    // Check if link is reachable
+
+    // Validate input as a URL
+    const regex =
+      /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+    if (!regex.test(link)) {
+      setError("Link is not a valid URL.");
+      return;
+    }
+
     try {
-      if (await isReachable(link)) {
+      const reachable = await urlExists(link);
+      if (reachable) {
+        localStorage.setItem(
+          "prevLinkArr",
+          JSON.stringify([...linkArr, { link: `//${link}`, linkTitle }])
+        );
         setLinkArr((prevLinkArr) => [
           ...prevLinkArr,
           { link: `//${link}`, linkTitle },
         ]);
+
         setError("");
         setLink("");
         setLinkTitle("");
       } else {
         throw new Error(`Error: ${link} is not reachable.`);
       }
-      // Catch error
     } catch (err) {
       console.error(err);
       setError("Link is not valid.");
@@ -43,7 +64,7 @@ function App() {
   }
 
   return (
-    <div className="p-5 relative h-screen max-w-[60rem] m-auto">
+    <div className="p-5 relative min-h-screen max-w-[60rem] m-auto">
       <h1 className="text-slate-800 text-2xl font-bold tracking-wide">
         Bookmark it.
       </h1>
@@ -56,10 +77,12 @@ function App() {
         linkTitle={linkTitle}
         setLinkTitle={setLinkTitle}
         handleSubmit={handleSubmit}
-        ClearAll={ClearAll}
       />
 
       <LinkList setLinkArr={setLinkArr} linkArr={linkArr} />
+
+      {/* hide delete all button if no links present */}
+      {linkArr.length > 0 && <DeleteAllLinks setLinkArr={setLinkArr} />}
     </div>
   );
 }
